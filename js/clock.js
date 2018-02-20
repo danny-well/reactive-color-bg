f3706bfcb00e6bd4fa24ba446a61aab6f1691e8e
Danny Well <danny@danny-well.com>
2018-02-19 19:35:43 -0500

    add moment.js and implement some clock logic

---
+++ js/clock.js
@@ -0,0 +1,125 @@
+$(function() {
+
+// THE BELOW SHOULD START TO FIX THE JUMPING INTERVAL
+
+	// var notPlaying = false;
+
+	// var default;
+
+	// function default() {
+	// 	default = setInterval(startClock, 1000;)
+	// }
+
+	// function startClock() {
+	// 		var nycDate = moment().tz("America/New_York").format('HH:mm:ss');
+	// 		if(notPlaying === false){
+	// 		$('.clock').html(nycDate);
+	// 		}
+	// 	};
+
+	// startClock();
+
+	//     function stopClock() {
+	//       clearInterval(default);
+	//       clearInterval();
+	//       clearInterval();
+	//     }
+
+function nycDate() {
+	setInterval(function() {
+	    var nycDate = moment().tz("America/New_York").format('HH:mm:ss');
+	    $('.clock').html(nycDate);
+	}, 1000);
+};
+
+function dubDate() {
+	setInterval(function() {
+	    var dubDate = moment().tz("Asia/Dubai").format('HH:mm:ss');
+	    $('.clock').html(dubDate);
+	}, 1000);
+};
+
+function aukDate() {
+	setInterval(function() {
+	    var aukDate = moment().tz("Pacific/Auckland").format('HH:mm:ss');
+	    $('.clock').html(aukDate);
+	}, 1000);
+};
+
+nycDate();
+
+	$("#one").on('click', function() {
+		notPlaying = true;
+		nycDate();
+	});
+
+	$("#two").on('click', function() {
+		notPlaying = true;
+		aukDate();
+	});
+
+	$("#three").on('click', function() {
+		notPlaying = true;
+		dubDate();
+	});
+
+
+
+
+
+// function addZero(i) {
+//     if (i < 10) {
+//         i = "0" + i;
+//     }
+//     return i;
+// }
+
+// setInterval(function() {
+//     var date = new Date();
+//     var h = addZero(date.getHours());
+//     var m = addZero(date.getMinutes());
+//     var s = addZero(date.getSeconds());
+//     $('.DUB').html(
+//         h + ":" + m + ":" + s
+//         );
+// }, 500);
+
+// function preventOverageDub(i) {
+//     if (i <= 14) {
+//         i = i + 9;
+//     }
+
+//     else {
+//     	i = i - 15;
+//     }
+
+//     return i;
+// }
+
+
+
+
+  // var decimal_places = 1;
+  // var decimal_factor = decimal_places === 0 ? 1 : decimal_places * 10;
+
+  // $('#target').animateNumber(
+  //   {
+  //     number: 100 * decimal_factor,
+  //     color: 'green',
+  //     'font-size': '30px',
+
+  //     numberStep: function(now, tween) {
+  //       var floored_number = Math.floor(now) / decimal_factor,
+  //           target = $(tween.elem);
+  //       if (decimal_places > 0) {
+  //         floored_number = floored_number.toFixed(decimal_places);
+  //       }
+
+  //       target.text(floored_number + ' %');
+  //     }
+  //   },
+  //   15000
+  // )
+
+
+});
--- js/scripts.js
+++ js/scripts.js
@@ -70,34 +70,53 @@ $(function() {
 
 	// Calculate angle of sunrise and sunset azimuth
 
-	function setAnimDelay1(){
-		var nycLat = 40.6782;
-		var nycLong = -73.9442;
-		var times = SunCalc.getTimes(new Date(), nycLat, nycLong);
-		var sunriseHour = times.sunrise.getHours();
-		var sunriseMin = times.sunrise.getMinutes();		
-		var nowHour = new Date().getHours();
-		var nowMin = new Date().getMinutes();
-		var hourDiff = sunriseHour - nowHour;
-		var minDiff = sunriseMin - nowMin;
-		var hoursAsPercent = hourDiff * 4.16; // Convert hours to percentage of day
-		var minsAsPercent = minDiff / 60 * 4.16; // Convert minutes to percentage of day
-		var totalPercent = hoursAsPercent + minsAsPercent; // Get full percentage
-		var animationDelay = nowHour >= 0 && nowHour < sunriseHour ? -86400 - ((totalPercent / 100 * 86400) * -1) : totalPercent / 100 * 86400; // Calculate animation delay
-		$("body").animate({animationDelay: animationDelay + "s"}, {duration: 1000, easing: "linear"});
-	}
-
-	function azimuth1 () {
+	function azimuth1() {
 		var nycLat = 40.6782;
 		var nycLong = -73.9442;
 		var times = SunCalc.getTimes(new Date(), nycLat, nycLong);
 		var sunrisePos = SunCalc.getPosition(times.sunrise, nycLat, nycLong);
 		var sunsetPos = SunCalc.getPosition(times.sunset, nycLat, nycLong);
-		var sunriseAzimuth = sunrisePos.azimuth * 180 / Math.PI;
-		var sunsetAzimuth = sunsetPos.azimuth * 180 / Math.PI;
+		var sunriseAzimuth = sunrisePos.azimuth;
+		var sunsetAzimuth = sunsetPos.azimuth;
+		var sunriseAzimuthConverted = sunrisePos.azimuth * 180 / Math.PI;
+		var sunsetAzimuthConverted = sunsetPos.azimuth * 180 / Math.PI;
 		console.log(sunriseAzimuth);
 		console.log(sunsetAzimuth);
+		console.log(sunriseAzimuthConverted);
+		console.log(sunsetAzimuthConverted);
 	}
+
+	function azimuth2() {
+		var aukLat = -36.8485;
+		var aukLong = 174.7633;
+		var times = SunCalc.getTimes(new Date(), aukLat, aukLong);
+		var sunrisePos = SunCalc.getPosition(times.sunrise, aukLat, aukLong);
+		var sunsetPos = SunCalc.getPosition(times.sunset, aukLat, aukLong);
+		var sunriseAzimuth = sunrisePos.azimuth;
+		var sunsetAzimuth = sunsetPos.azimuth
+		var sunriseAzimuthConverted = sunrisePos.azimuth * 180 / Math.PI;
+		var sunsetAzimuthConverted = sunsetPos.azimuth * 180 / Math.PI;
+		console.log(sunriseAzimuth);
+		console.log(sunsetAzimuth);
+		console.log(sunriseAzimuthConverted);
+		console.log(sunsetAzimuthConverted);
+	}	
+
+	function azimuth3() {
+		var dubLat = 25.2048;
+		var dubLong = 55.2708;
+		var times = SunCalc.getTimes(new Date(), dubLat, dubLong);
+		var sunrisePos = SunCalc.getPosition(times.sunrise, dubLat, dubLong);
+		var sunsetPos = SunCalc.getPosition(times.sunset, dubLat, dubLong);
+		var sunriseAzimuth = sunrisePos.azimuth;
+		var sunsetAzimuth = sunsetPos.azimuth;
+		var sunriseAzimuthConverted = sunrisePos.azimuth * 180 / Math.PI;
+		var sunsetAzimuthConverted = sunsetPos.azimuth * 180 / Math.PI;
+		console.log(sunriseAzimuth);
+		console.log(sunsetAzimuth);
+		console.log(sunriseAzimuthConverted);
+		console.log(sunsetAzimuthConverted);
+	}	
 
 
 	// Calculate aspect ratio of window
